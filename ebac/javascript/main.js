09a0c78714e1abeb92d2e3432a00bafe0a16665b
Elson Mateus <111660816+ElsonMateusFE@users.noreply.github.com>
2022-11-24 20:04:25 -0300

    atualizacao

--- ebac/javascript/main.js
+++ ebac/javascript/main.js
@@ -1,44 +1,36 @@
-const form = document.getElementById('form-deposito');
-const nomeBeneficiario = document.getElementById('nome-beneficiario');
-let formEValido = false;
+const form = document.getElementById('form-deposito')
+const nomeBeneficiario = document.getElementById('nome-beneficiario')
+let validForm = false
 
-function validaNome(nomeCompleto){
-    const nomeComoArray = nomeCompleto.split(' ');
-    return nomeComoArray.length >= 2;
+form.addEventListener('submit', function(evento){
+    evento.preventDefault()
+
+    const numeroConta = document.getElementById('numero-conta')
+    const valorDeposito = document.getElementById('valor-deposito')
+    const mensagemSucesso = `${nomeBeneficiario.value} ${numeroConta.value} ${valorDeposito.value}`
+
+    validForm = nomeValid(nomeBeneficiario.value)
+    if(validForm){
+        document.querySelector('.success-message').style.display = 'block'
+        document.querySelector('.success-message').innerHTML = mensagemSucesso
+    }else{
+        document.querySelector('.error-message').style.display = 'block'
+    }
+})
+
+nomeBeneficiario.addEventListener('keyup', function(){
+    validForm = nomeValid(nomeBeneficiario.value)
+    if(!validForm){
+        nomeBeneficiario.classList.add('error')
+        document.querySelector('.error-message').style.display = 'block'
+    }else{
+        document.querySelector('.error-message').style.display = 'none'
+        nomeBeneficiario.classList.remove('error')
+    }
+})
+
+function nomeValid(nomeCompleto){
+    const nomeSplit = nomeCompleto.split(' ')
+
+    return nomeSplit.length >= 2
 }
-
-form.addEventListener('submit', function(e){
-    e.preventDefault();
-
-    const numeroContaBeneficiario = document.getElementById('numero-conta');
-    const valorDeposito = document.getElementById('valor-deposito');
-    const mensagemSucesso = `Montante de: <strong>${valorDeposito.value}</strong> depositado para o cliente: <strong>${nomeBeneficiario.value}</strong> - conta: <strong>${numeroContaBeneficiario.value}</strong>`;
-
-    formEValido = validaNome(nomeBeneficiario.value)
-    if(formEValido){
-        const containerMensagemSucesso = document.querySelector('.success-message');
-        containerMensagemSucesso.innerHTML = mensagemSucesso;
-        containerMensagemSucesso.style.display = 'block';
-
-        nomeBeneficiario.value = '';
-        numeroContaBeneficiario.value = '';
-        valorDeposito.value = '';
-    }else{
-        nomeBeneficiario.style.border = '1px solid red';
-        document.querySelector('.error-message').style.display = 'block';
-    }
-})
-
-nomeBeneficiario.addEventListener('keyup', function(e){
-    console.log(e.target.value);
-    formEValido = validaNome(nomeBeneficiario.value);
-
-    if(!formEValido){
-        nomeBeneficiario.classList.add('error');
-        // nomeBeneficiario.style.border = '1px solid red';
-        document.querySelector('.error-message').style.display = 'block';
-    }else{
-        nomeBeneficiario.classList.remove('error');
-        document.querySelector('.error-message').style.display = 'none';
-    }
-})
