e0f025159f6875a4cdc561124877a95869d31f08
Elson Mateus <111660816+ElsonMateusFE@users.noreply.github.com>
2022-12-05 14:33:10 -0300

    atualizacao

--- ebac/javascript/revisao.js
+++ ebac/javascript/revisao.js
@@ -2,35 +2,31 @@ const form = document.getElementById('form-deposito')
 const nomeBeneficiario = document.getElementById('nome-beneficiario')
 let validaForm = false
 
-form.addEventListener('submit', function(evento){
-    evento.preventDefault()
+form.addEventListener('submit', function(event){
+    event.preventDefault()
 
     const numeroConta = document.getElementById('numero-conta')
     const valorDeposito = document.getElementById('valor-deposito')
-    const mensagemSucesso = `${nomeBeneficiario.value} da conta ${numeroConta.value} tem R$ ${valorDeposito.value}`
+    const mensagemSucesso = `${nomeBeneficiario.value}, ${numeroConta.value}, ${valorDeposito.value}`
 
     validaForm = validaNome(nomeBeneficiario.value)
     if(validaForm){
-        //alert(mensagemSucesso)
         document.querySelector('.success-message').style.display = 'block'
-        document.querySelector('.success-message').innerText = mensagemSucesso
+        document.querySelector('.success-message').innerHTML = mensagemSucesso
     }else{
-        //alert('Nome incompleto')
-        nomeBeneficiario.style.border = '2px solid red'
         document.querySelector('.error-message').style.display = 'block'
+        nomeBeneficiario.style.border = '2px solid red'
     }
 })
 
 nomeBeneficiario.addEventListener('keyup', function(){
-    //console.log(evento)
     validaForm = validaNome(nomeBeneficiario.value)
     if(!validaForm){
-        //alert('Nome incompleto')
-        nomeBeneficiario.classList.add('error')
         document.querySelector('.error-message').style.display = 'block'
+        nomeBeneficiario.classList.add('error')
     }else{
-        nomeBeneficiario.classList.remove('error')
         document.querySelector('.error-message').style.display = 'none'
+        nomeBeneficiario.classList.remove('error')
     }
 })
 
