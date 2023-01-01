f3d05f4388f2c0f680353c4ec01e086135df81c5
Elson Mateus <111660816+ElsonMateusFE@users.noreply.github.com>
2022-12-31 21:10:04 -0300

    atualizacao

--- ebac/ajax/fetch.js
+++ ebac/ajax/fetch.js
@@ -1,3 +1,4 @@
+/*
 $(document).ready(function(){
     $('#cep').mask('00000-000', {
         placeholder: '00000-000'
@@ -26,10 +27,66 @@ $(document).ready(function(){
 
             $('#endereco').val(endereco)
 
-            setInterval(function(){
+            setTimeout(function(){
                 $(botao).find('i').removeClass('d-none')
                 $(botao).find('span').addClass('d-none')
             }, 4000)
         })
     })
+})
+*/
+$(document).ready(function(){
+    $('#cep').mask('00000-000', {
+        placeholder: '00000-000'
+    })
+
+    $('#btn-buscar-cep').click(function(){
+        const cep = $('#cep').val()
+        const endpoint = `https://viacep.com.br/ws/${cep}/json`
+        const botao = $(this)
+        $(botao).find('i').addClass('d-none')
+        $(botao).find('span').removeClass('d-none')
+
+        //window.fetch()
+        fetch(endpoint)
+        .then(function(resposta){
+            //console.log(resposta)
+            return resposta.json()
+        })
+        .then(function(json){
+            console.log(json)
+            const logradouro = json.logradouro
+            const bairro = json.bairro
+            const cidade = json.localidade
+            const estado = json.uf
+            const endereco = `${logradouro}, ${bairro} - ${cidade} - ${estado}`
+            $('#endereco').val(endereco)
+        })
+        .catch(function(erro){
+            alert('Ocorreu um erro ao buscar o endereço, tente novamente mais tarde')
+        })
+        .finally(function(){
+            setTimeout(function(){
+                $(botao).find('i').removeClass('d-none')
+                $(botao).find('span').addClass('d-none')
+            }, 1000)
+        })
+    })
+
+    /*
+    $('#formulario-pedido').submit(function(evento){
+        evento.preventDefault()
+
+        if($('#nome').val() == 0){
+            throw new Error('Digite o nome')
+        }
+    })
+    */
+  $('#formulario-pedido').submit((evento) => {
+    evento.preventDefault()
+
+    if($('#nome').val() == 0){
+        throw new Error('Escreva um nome')
+    }
+  })
 })
