6666e3f8c89b7a8e7422c7e4eb56e44d275dfc33
Elson Mateus <111660816+ElsonMateusFE@users.noreply.github.com>
2023-02-06 20:00:02 -0300

    atualizacao

--- ebac_javascript/gulp/gulpfile.js
+++ ebac_javascript/gulp/gulpfile.js
@@ -3,9 +3,15 @@ const concat = require('gulp-concat')
 const cssmin = require('gulp-cssmin')
 const rename = require('gulp-rename')
 const uglify = require('gulp-uglify')
+const image = require('gulp-imagemin')
 
 function tarefasCSS(cb){
-    return gulp.src('vendor/**/*.css')
+    return gulp.src([
+        'node_modules/bootstrap/dist/css/bootstrap-grid.min.css',
+        'vendor/owl/css/owl.css',
+        'node_modules/@fortawesome/fontawesome-free/css/fontawesome.css',
+        'vendor/jquery-ui/jquery-ui.min.css'
+    ])
         .pipe(concat('libs.css'))
         .pipe(cssmin())
         .pipe(rename({ suffix: 'min' }))
@@ -20,5 +26,22 @@ function tarefasJS(){
         .pipe(gulp.dest('dist/js'))
 }
 
+function tarefaImagem(){
+    return gulp.src('src/images/*')
+        .pipe(image({
+            pngquant: true,
+            optipng: false,
+            zopflipng: true,
+            jpegRecompress: false,
+            mozjpeg: true,
+            gifsicle: true,
+            svgo: true,
+            concurrent: 10,
+            quiet: true   
+        }))
+        .pipe(gulp.dest('dist/images'))
+}
+
 exports.styles = tarefasCSS
 exports.scripts = tarefasJS
+exports.images = tarefaImagem
