d88d071bd51d007a200d7388cf7083c820403517
Elson Mateus <111660816+ElsonMateusFE@users.noreply.github.com>
2023-03-07 10:39:54 -0300

    atualizacao

---
+++ ebac/revisao/src/components/Titulo/styles.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+import { Props } from '.'
+
+export const Titulo = styled.h3<Props>`
+  color: black;
+  font-size: 14px;
+  font-weight: bold;
+  margin-bottom: 16px;
+`
